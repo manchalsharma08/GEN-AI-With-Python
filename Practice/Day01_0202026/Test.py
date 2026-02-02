db582d32e05351e734e6aaf1f510f4678cf9e0ca
manchal <manchalsharma08@gmail.com>
2026-02-03 00:09:18 +0530

    FIRSTDAY

---
+++ Practice/Day01_0202026/Test.py
@@ -0,0 +1,48 @@
+# Addition 
+a = 10
+b = 20
+c = a + b
+print(c)
+
+# Type of Data Show
+a = "my name manchal"
+print(a)
+
+b=a
+print(b)
+
+b= 3
+print(b)
+
+# string
+
+line = "manchal's son is yadvik"
+z = line
+print(z)
+
+# for Function
+
+name = "AI"
+for i in range(3):
+ print(name)
+
+
+# write a Table of 2
+print("## 01")
+for i in range(1,11):
+  print("2x",i,"=",2*i)
+
+print("## 02")
+print("## Takeing input from User")
+
+Table = int(input("Enter Number: "))
+for i in range(1,11):
+  print(Table,"x",i,"=",Table*i)
+
+print("## 03")
+print("## While loop Function for Table Write")
+
+i = 1
+while i <= 10:
+  print("2x",i,"=",2*i)
+  i += 1
